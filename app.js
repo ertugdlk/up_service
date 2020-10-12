409772e5e71fc030df9bebe246631a915e132910
Ertug Dilek <ertgdlk@gmail.com>
2020-10-12 14:32:09 +0300

    Service created

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const Express = require('express')
 const BodyParser = require('body-parser')
 const Config     = require('config')
 const Cors = require('cors')
+const Mongoose = require('mongoose')
 
 const App = Express()
 
@@ -9,7 +10,28 @@ App.use(BodyParser.json())
 App.use(BodyParser.urlencoded({ extended: true }))
 App.use(Cors())
 
-console.log('# --> Port:', Config.port())
+//DATABASE
+const dbUrl = Config.get('database.url');
+Mongoose.set('useFindAndModify', false) // FindAndModify method is deprecated. If this line is not exists, then it throws error.
+Mongoose.set('useCreateIndex', true) // ensureIndex is deprecated. If this line is not exists, then it throws error.
+Mongoose.connect(dbUrl)
 
-App.listen(Config.port())
+Mongoose.connection.on('open', (d) => 
+{
+    console.log('# --> Database connection is opened. (DB)')
+})
+Mongoose.connection.on("error", function(err) {
+    console.log("Could not connect to mongo server!");
+    return console.log(err);
+});
+
+//Service
+App.listen((process.env.PORT || 5000), () => {
+    console.log('# --> Service connection is opened. (App.js)')
+})
+App.get( '/' , (req,res) => res.send('Hello, welcome to unkownpros API service'));
+
+//Routes
+//App.use('/users' , require('./Routes/UserRoute')) 
+
 
--- config/default.js
+++ config/default.js
@@ -5,7 +5,7 @@ const defaultConfig =
     "port": () => process.env.PORT || 5555,
 	"database":
 	{
-
+        "url":"mongodb+srv://upErtug:rIkHZnclXxS2sdQe@staging.qyf6n.mongodb.net/stage?retryWrites=true&w=majority"
     }
 }
 
