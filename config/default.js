1df4338b466927ae90035bb73d31623ade756e51
Ertug Dilek <ertgdlk@gmail.com>
2020-10-14 16:18:04 +0300

    DB settings changed

--- config/default.js
+++ config/default.js
@@ -2,7 +2,7 @@ const defaultConfig =
 {
 	"database":
 	{
-        "url":"mongodb+srv://upErtug:rIkHZnclXxS2sdQe@upcluster.qyf6n.mongodb.net/test?retryWrites=true&w=majority"
+        "url":"mongodb+srv://"+process.env.NODE_DATABASE_USERNAME+":"+process.env.NODE_DATABASE_PASSWORD+"@upcluster.qyf6n.mongodb.net/test?retryWrites=true&w=majority"
     }
 }
 
