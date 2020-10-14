f709c4c26700f37d094e6f2753936766d6071317
Ertug Dilek <ertgdlk@gmail.com>
2020-10-14 16:41:40 +0300

    AuthenticationValidation middleware file created

---
+++ middlewares/AuthenticationValidation.js
@@ -0,0 +1,16 @@
+const _ = require('lodash')
+
+class AuthenticationValidation
+{
+    static async isJWTVerified(req, res, next)
+    {
+        try
+        {
+
+        }
+        catch
+        {
+
+        }
+    }
+}
