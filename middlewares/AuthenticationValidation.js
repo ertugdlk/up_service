0938b32f3ec26703293d161be2eb03eef5179f22
Ertug Dilek <ertgdlk@gmail.com>
2020-10-15 11:39:08 +0300

    AuthValidation middleware added

--- middlewares/AuthenticationValidation.js
+++ middlewares/AuthenticationValidation.js
@@ -1,4 +1,7 @@
 const _ = require('lodash')
+const Config = require('config')
+const JWTutil = require('up_core/utils/JWTutil')
+const Boom = require('@hapi/boom')
 
 class AuthenticationValidation
 {
@@ -6,11 +9,27 @@ class AuthenticationValidation
     {
         try
         {
+            const jwtOptions = Config.get('jwt')
+            const accessToken = JWTutil.getAccessToken(req)
 
+            const user = await JWTutil.verify(accessToken, jwtOptions)
+
+            if(user)
+            {
+                res.locals.userId = user._id
+
+                next()
+            }
+            else
+            {
+                Boom.unauthorized('Access token is invalid')
+            }
         }
-        catch
+        catch(error)
         {
-
+            next(Boom.conflict('No permission to access'))
         }
     }
 }
+
+module.exports = AuthenticationValidation
