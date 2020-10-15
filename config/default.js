7d69382c3d37aadb7f020633510c867978710991
Ertug Dilek <ertgdlk@gmail.com>
2020-10-15 10:53:07 +0300

    JWT config settings created

--- config/default.js
+++ config/default.js
@@ -3,7 +3,16 @@ const defaultConfig =
 	"database":
 	{
         "url":"mongodb+srv://"+process.env.NODE_DATABASE_USERNAME+":"+process.env.NODE_DATABASE_PASSWORD+"@upcluster.qyf6n.mongodb.net/test?retryWrites=true&w=majority"
-    }
+    },
+    "jwt":
+	{
+		"secret": "cukubik",
+        "expiresIn": "7 days",
+        "is": 
+        {
+            "active": true // To make token system disable, set this to `false`
+        }
+	}
 }
 
 module.exports = defaultConfig
