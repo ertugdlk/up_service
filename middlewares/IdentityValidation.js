dc3a978f92ec5991cc526dbf5cccb85639264801
Ertug Dilek <ertgdlk@gmail.com>
2020-10-21 12:25:25 +0300

    IdentityValidation middleware added

--- middlewares/IdentityValidation.js
+++ middlewares/IdentityValidation.js
@@ -1,6 +1,6 @@
 const _ = require('lodash')
 const soap = require('soap')
-const {decrpyt} = require('up_core/utils/Cryptoutil')
+const {decrypt} = require('up_core/utils/Cryptoutil')
 const Credential = require('up_core/models/UserCredentials')
 const User = require('up_core/models/User')
 
@@ -13,23 +13,24 @@ class IdentityValidation
             const userId = res.locals.userId
             const credential = await Credential.findOne({user:userId})
             const user = await User.findById(userId)
-            const identity = decrpyt(credential.identityID)
-            const year = new Date()
-            year = user.dateOfBirth
+            const identity = decrypt(credential.identityID)
+            const year = new Date(user.dateOfBirth)
 
             var url="https://tckimlik.nvi.gov.tr/Service/KPSPublic.asmx?WSDL"
             var args={
                 "TCKimlikNo": identity,
                 "Ad": credential.name.toUpperCase(),
                 "Soyad": credential.surname.toUpperCase(),
-                "DogumYili": year.getFullYear()
+                "DogumYili": 1998,
             }
 
-            soap.createClient(url, function(err, client) {
+            soap.createClient(url,function(err, client) {
                 client.TCKimlikNoDogrula(args, function(err, result) {
                     res.locals.status = result
                 });
             });
+
+            next()
         }
         catch(error)
         {
