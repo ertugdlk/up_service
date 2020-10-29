2ca26c17d8357f746808c7e73e6fefeeca59829c
Ertug Dilek <ertgdlk@gmail.com>
2020-10-29 10:55:50 +0300

    SteamController createsteamdetail refactored

--- controllers/SteamController.js
+++ controllers/SteamController.js
@@ -1,6 +1,8 @@
 const _ = require('lodash')
 
 const Detail = require('up_core/models/Detail')
+const SteamAPI = require('up_core/models/steam/SteamAPI')
+const SteamUserDetail = require('up_core/models/steam/SteamUserDetail')
 
 class SteamController {
     static async createSteamDetail(req,res,next)
@@ -10,9 +12,23 @@ class SteamController {
             const split = req.query["openid.claimed_id"].split('/')
             const steamID = split[split.length - 1]
 
+            const SteamDetail = await SteamUserDetail.find(steamID)
+            const clearedDetail = SteamDetail.toDetail()
+            const userDetail = new Detail(clearedDetail)
+            await userDetail.save()
+            
+        }
+        catch(error)
+        {
+            throw error
+        }
+    }
 
+    static async getSteamGames(req,res,next)
+    {
+        try
+        {
 
-            console.log(steamID)
         }
         catch(error)
         {
