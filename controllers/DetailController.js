40fce12225a4275195519ae8295f49edae9a7d08
Ertug Dilek <ertgdlk@gmail.com>
2020-11-04 11:49:43 +0300

    DetailController usergames method refactor

--- controllers/DetailController.js
+++ controllers/DetailController.js
@@ -9,14 +9,11 @@ class DetailController {
       const Details = await Detail.find({ user: "5f96872bf30677640e4ab2cb" })
       const Games = _.map(Details, 'games')
       const AllGames = _.flattenDeep(Games)
-      Game.find({
+      const result = await Game.find({
         '_id': { $in: AllGames }
-      }, function (err, gameData) {
-        res.send(gameData);
-      });
+      })
 
-      //res.send(AllGames)
-
+      res.send(result)
     }
     catch (error) {
       throw error
