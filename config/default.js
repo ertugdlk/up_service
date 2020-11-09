8100792a3d841e59c504bbea69f0ed73746aff33
Ertug Dilek <ertgdlk@gmail.com>
2020-11-09 15:14:57 +0300

    last changes

--- config/default.js
+++ config/default.js
@@ -21,9 +21,9 @@ const defaultConfig =
     {
         "steam":
         {
-            name:"Steam",
-            apiKey: "3F7E7FF7EC5EC88290ECF9ED3C63F642",
-            _id : '5f9a84fca1f0c0b83de7d696'
+            "name":"Steam",
+            "apiKey": "3F7E7FF7EC5EC88290ECF9ED3C63F642",
+            "_id" : '5f9a84fca1f0c0b83de7d696'
         }
     }
 }
