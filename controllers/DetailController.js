846fe08bd6c0e281512b5574db198f926016ebfa
erce <canerce99@hotmail.com>
2020-11-11 20:11:39 +0300

    setign

--- controllers/DetailController.js
+++ controllers/DetailController.js
@@ -20,6 +20,30 @@ class DetailController {
     }
   }
 
+  static async setIgn(req, res, next) {
+    try {
+      const Ign =
+        _.chain(req.body)
+          .pick(['ign'])
+          .value()
+
+      const gameId =
+        _.chain(req.body)
+          .pick(['appID'])
+          .value()
+      const Details = await Detail.find({ user: res.locals.userId })
+      _games = Details.games
+      const filter = { id: gameId }
+      const update = { ign: Ign }
+      const _game = await _games.findOneAndUpdate(filter, update)
+      //const _game = await Detail.find({ ObjectId: gameId })
+      //_game.set({ ign: Ign })
+      await _game.save()
+    }
+    catch (error) {
+      throw error
+    }
+  }
 }
 
 module.exports = DetailController
