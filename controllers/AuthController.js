529ee14d1d8fa8e07035b07fd98ee16346cb257d
erce <canerce99@hotmail.com>
2020-12-05 13:01:59 +0300

    email verification controllers

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,6 +1,9 @@
 const _ = require('lodash')
 const Redis = require('redis')
 const User = require('up_core/models/User')
+import { response } from 'express'
+import { findOne } from 'up_core/models/User'
+import { sendOtp, verifyOtp } from 'up_core/utils/emailVerification'
 
 class AuthController {
     static async createUser(req, res, next) {
@@ -11,15 +14,13 @@ class AuthController {
                     .value()
 
             const nicknameCheck = await User.findOne({ nickname: mappedUser.nickname })
-            const emailCheck = await User.findOne({email : mappedUser.email})
+            const emailCheck = await User.findOne({ email: mappedUser.email })
 
-            if(nicknameCheck)
-            {
-                return res.status(203).send({status: 0 , msg: 'Exist User Nickname'})
+            if (nicknameCheck) {
+                return res.status(203).send({ status: 0, msg: 'Exist User Nickname' })
             }
-            else if(emailCheck)
-            {
-                return res.status(203).send({status: 0 , msg: 'Exist User Email'})
+            else if (emailCheck) {
+                return res.status(203).send({ status: 0, msg: 'Exist User Email' })
             }
 
             const user = await new User(mappedUser)
@@ -28,6 +29,36 @@ class AuthController {
             return res.send(user)
         }
         catch (error) {
+            throw error
+        }
+    }
+
+    static async sendOTP(req, res, nrext) {
+        try {
+            if (req.body.email) {
+                const otp = await sendOtp(req.body.email)
+            } else {
+                return ({ "msg": "Enter an email", "status": "0" })
+            }
+        } catch (error) {
+            throw error
+        }
+    }
+
+    static async verifyOTP(email, otp) {
+        try {
+            verifyOtp(email, otp, (err, data) => {
+                const result = data.status
+                if (result != 1) {
+                    return ({ "msg": "Wrong or old OTP", "status": "0" })
+                } else {
+                    const user = await findOneAndUpdate({ emai: email })
+                    user.emailVerified = true
+                    return response(true)
+                }
+            })
+
+        } catch (error) {
             throw error
         }
     }
@@ -41,9 +72,8 @@ class AuthController {
 
             const user = await User.findOne({ nickname: mappedCredentials.nickname })
 
-            if(!user)
-            {
-                return res.send({err:"user not found"})
+            if (!user) {
+                return res.send({ err: "user not found" })
             }
 
             if (!user.findByCredentials(mappedCredentials.password)) {
@@ -51,7 +81,7 @@ class AuthController {
             }
 
             const token = await user.generateAuthToken()
-            res.cookie('token', token, { httpOnly: true , maxAge: 7*24*6*604800});
+            res.cookie('token', token, { httpOnly: true, maxAge: 7 * 24 * 6 * 604800 });
             res.send('success')
         }
         catch (error) {
@@ -59,12 +89,12 @@ class AuthController {
         }
     }
 
-    static async logoutUser(req,res,next) {
-        try{
-            res.cookie('token', '', { maxAge: 0})
+    static async logoutUser(req, res, next) {
+        try {
+            res.cookie('token', '', { maxAge: 0 })
             res.end()
         }
-        catch(error){
+        catch (error) {
             throw error
         }
     }
