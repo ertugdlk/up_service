878b4e47dd8c9550788d8e4e5e7f4fc0065196b0
Ertug Dilek <ertgdlk@gmail.com>
2020-12-09 10:52:27 +0300

    Erce merge

--- routes/GameRoomRoute.js
+++ routes/GameRoomRoute.js
@@ -4,7 +4,8 @@ const Authentication = require('../middlewares/AuthenticationValidation')
 const router = require('express').Router()
 
 router.get('/getone', controller.getRoomData)
-router.get('/getall', Authentication.isJWTVerified,
-    controller.getRoomsData)
+router.get('/getall', 
+    Authentication.isJWTVerified,
+        controller.getRoomsData)
 
 module.exports = router
