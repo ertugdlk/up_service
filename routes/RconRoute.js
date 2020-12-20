e2aceafd687c06abf83667d820d3cfea997f1f47
Ertug Dilek <ertgdlk@gmail.com>
2020-12-20 16:57:16 +0300

    deneme

--- routes/RconRoute.js
+++ routes/RconRoute.js
@@ -2,7 +2,7 @@ const controller = require('../controllers/RconController')
 const router = require('express').Router()
 
 router.get('/status' ,controller.status)
-router.get('/matchconfig', controller.loadMatchSettings)
+router.get('/matchconfig',  controller.loadMatchSettings)
 router.post('/setupmatch' , controller.setupMatchSettings)
 router.post('/startmatch' , controller.createMatch)
 
