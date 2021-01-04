f35dc722f41bc341d50cbfb2ebcd24deab7564da
Ertug Dilek <ertgdlk@gmail.com>
2021-01-04 15:45:59 +0300

    steamcontroller error fix

--- controllers/SteamController.js
+++ controllers/SteamController.js
@@ -7,50 +7,48 @@ const SteamAPI = require('up_core/models/steam/SteamAPI')
 const SteamUserDetail = require('up_core/models/steam/SteamUserDetail')
 
 class SteamController {
-    static async getSteamID(req,res,next)
-    {
-        try
-        {
-            if(!req.query["openid.claimed_id"])
-            {
-                res.status(502).json({msg: failed})
+    static async getSteamID(req, res, next) {
+        try {
+            if (!req.query["openid.claimed_id"]) {
+                res.status(502).json({ msg: failed })
             }
-            else
-            {
+            else {
                 const split = req.query["openid.claimed_id"].split('/')
                 const steamID = split[split.length - 1]
                 res.locals.steamId = steamID
                 next()
-                
+
             }
         }
-        catch(error)
-        {
-            res.status(500).json({msg: 'failed'})
+        catch (error) {
+            res.status(500).json({ msg: 'failed' })
             throw error
         }
     }
 
-    static async getSteamGames(req,res,next)
-    {
-        try
-        {             
+    static async getSteamGames(req, res, next) {
+        try {
             const steamID = res.locals.steamId
-            const response = await SteamUserDetail.find({steamID : steamID})
+            const response = await SteamUserDetail.find({ steamID: steamID })
             const SteamDetail = new SteamUserDetail(response)
 
-            const clearedDetail = SteamDetail.toDetail({user : res.locals.userId})
+            const clearedDetail = SteamDetail.toDetail({ user: res.locals.userId })
             const userDetail = new Detail(clearedDetail.__wrapped__)
-            
+
             //error over here
-            const detail = await SteamUserDetail.matchGames({steamID: steamID, user: res.locals.userId , detail: userDetail})
-            await detail.save()
-            
-            res.redirect('https://ertug-2.d4u99xidnqjcw.amplifyapp.com/dashboard/' + detail.name)
+            const detail = await SteamUserDetail.matchGames({ steamID: steamID, user: res.locals.userId, detail: userDetail })
+            if (detail == null) {
+                res.redirect('https://ertug-2.d4u99xidnqjcw.amplifyapp.com/dashboard/')
+                res.end()
+            }
+            else {
+                await detail.save()
+                res.redirect('https://ertug-2.d4u99xidnqjcw.amplifyapp.com/dashboard/' + detail.name)
+            }
         }
-        catch(error)
-        {
-            res.status(500).json({message: "Incorrect authorization token"})
+        catch (error) {
+            res.status(500).json({ message: "Incorrect authorization token" })
+            res.redirect('https://ertug-2.d4u99xidnqjcw.amplifyapp.com/dashboard/')
             throw error
         }
     }
