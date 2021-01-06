ea6c00a4327d8d7b581c66a9ab811efb12e71fdf
Ertug Dilek <ertgdlk@gmail.com>
2021-01-06 21:50:50 +0300

    ertug merge

--- controllers/SteamController.js
+++ controllers/SteamController.js
@@ -1,57 +1,59 @@
-const _ = require('lodash')
-var LocalStorage = require('node-localstorage').LocalStorage
-const localStorage = new LocalStorage('./scratch')
+const _ = require("lodash")
+var LocalStorage = require("node-localstorage").LocalStorage
+const localStorage = new LocalStorage("./scratch")
 
-const Detail = require('up_core/models/Detail')
-const SteamAPI = require('up_core/models/steam/SteamAPI')
-const SteamUserDetail = require('up_core/models/steam/SteamUserDetail')
+const Detail = require("up_core/models/Detail")
+const SteamAPI = require("up_core/models/steam/SteamAPI")
+const SteamUserDetail = require("up_core/models/steam/SteamUserDetail")
 
 class SteamController {
-    static async getSteamID(req, res, next) {
-        try {
-            if (!req.query["openid.claimed_id"]) {
-                res.status(502).json({ msg: failed })
-            }
-            else {
-                const split = req.query["openid.claimed_id"].split('/')
-                const steamID = split[split.length - 1]
-                res.locals.steamId = steamID
-                next()
-
-            }
-        }
-        catch (error) {
-            res.status(500).json({ msg: 'failed' })
-            throw error
-        }
+  static async getSteamID(req, res, next) {
+    try {
+      if (!req.query["openid.claimed_id"]) {
+        res.status(502).json({ msg: failed })
+      } else {
+        const split = req.query["openid.claimed_id"].split("/")
+        const steamID = split[split.length - 1]
+        res.locals.steamId = steamID
+        next()
+      }
+    } catch (error) {
+      res.status(500).json({ msg: "failed" })
+      throw error
     }
+  }
 
-    static async getSteamGames(req, res, next) {
-        try {
-            const steamID = res.locals.steamId
-            const response = await SteamUserDetail.find({ steamID: steamID })
-            const SteamDetail = new SteamUserDetail(response)
+  static async getSteamGames(req, res, next) {
+    try {
+      const steamID = res.locals.steamId
+      const response = await SteamUserDetail.find({ steamID: steamID })
+      const SteamDetail = new SteamUserDetail(response)
 
-            const clearedDetail = SteamDetail.toDetail({ user: res.locals.userId })
-            const userDetail = new Detail(clearedDetail.__wrapped__)
+      const clearedDetail = SteamDetail.toDetail({ user: res.locals.userId })
+      const userDetail = new Detail(clearedDetail.__wrapped__)
 
-            //error over here
-            const detail = await SteamUserDetail.matchGames({ steamID: steamID, user: res.locals.userId, detail: userDetail })
-            if (detail == null) {
-                res.redirect('https://ertug-2.d4u99xidnqjcw.amplifyapp.com/dashboard/')
-                res.end()
-            }
-            else {
-                await detail.save()
-                res.redirect('https://ertug-2.d4u99xidnqjcw.amplifyapp.com/dashboard/' + detail.name)
-            }
-        }
-        catch (error) {
-            res.status(500).json({ message: "Incorrect authorization token" })
-            res.redirect('https://ertug-2.d4u99xidnqjcw.amplifyapp.com/dashboard/')
-            throw error
-        }
+      //error over here
+      const detail = await SteamUserDetail.matchGames({
+        steamID: steamID,
+        user: res.locals.userId,
+        detail: userDetail,
+      })
+      if (detail == null) {
+        res.redirect("https://ertug-2.d4u99xidnqjcw.amplifyapp.com/dashboard/")
+        res.end()
+      } else {
+        await detail.save()
+        res.redirect(
+          "https://ertug-2.d4u99xidnqjcw.amplifyapp.com/dashboard/" +
+            detail.name
+        )
+      }
+    } catch (error) {
+      res.status(500).json({ message: "Incorrect authorization token" })
+      res.redirect("https://ertug-2.d4u99xidnqjcw.amplifyapp.com/dashboard/")
+      throw error
     }
+  }
 }
 
 module.exports = SteamController
