d741c1976a5f4004d1d2774fdde71a3fd1280526
ertugdlk <ertgdlk@gmail.com>
2021-02-04 18:36:43 +0300

    Erce merge wallet and balance

--- controllers/WalletController.js
+++ controllers/WalletController.js
@@ -1,22 +1,23 @@
 const Balance = require("up_core/models/Balance")
 const User = require("up_core/models/User")
 
-
 class WalletController {
-
-    static async getBalance(req, res, next) {
-        const user = await User.findOne({ nickname: req.body.nickname })
-        if (!user) {
-            res.send({ status: 0, msg: "ERROR no user found(get balance endpoint)" })
-            res.end()
-        }
-        const wallet = await Balance.findOne({ user: user._id })
-        if (!wallet) {
-            res.send({ status: 0, msg: "ERROR no wallet found with the given user information." })
-        }
-
-        res.send({ balance: wallet.balance, msg: "Returning wallet balance." })
+  static async getBalance(req, res, next) {
+    const user = await User.findOne({ _id: res.locals.userId })
+    if (!user) {
+      res.send({ status: 0, msg: "ERROR no user found" })
+      res.end()
     }
+    const wallet = await Balance.findOne({ user: user._id })
+    if (!wallet) {
+      res.send({
+        status: 0,
+        msg: "No wallet found with the given user information.",
+      })
+    }
+
+    res.send({ balance: wallet.balance, msg: "Success wallet balance." })
+  }
 }
 
 module.exports = WalletController
