253f2e3a88f2e02b55be4828ef7c0113fab30a1a
erce <canerce99@hotmail.com>
2021-03-26 13:34:36 +0300

    pay2d fix

--- routes/PaymentRoute.js
+++ routes/PaymentRoute.js
@@ -3,6 +3,6 @@ const Controller = require("../controllers/Pay2DController")
 const Authentication = require("../middlewares/AuthenticationValidation")
 
 //Routes
-router.post("/pay2d",Authentication.isJWTVerified,  Controller.paySmart2D)
+router.post("/pay2d",  Controller.paySmart2D)
 
 module.exports = router
