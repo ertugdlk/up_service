5fec0bc822aff92e0f65db6826ed65b20eb3e9aa
ertugdlk <ertgdlk@gmail.com>
2021-05-01 16:24:38 +0300

    pm2 settings#2

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -8,8 +8,6 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
-npm i -g pm2
-
 #navigate into our working directory where we have all our bitbucket files
 cd /home/ec2-user/up_service
 pm2 stop 0
