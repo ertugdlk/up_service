fcd13bb3b1083fe0e51e59cd7a990f7b31134aa0
ertugdlk <ertgdlk@gmail.com>
2021-05-01 16:20:06 +0300

    last settings

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -8,7 +8,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
-export PATH=$PATH:/opt/node/lib/node_modules/pm2/bin
+npm i -g pm2
 
 #navigate into our working directory where we have all our bitbucket files
 cd /home/ec2-user/up_service
