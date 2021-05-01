27e919d1047d326092dd7a87c839050d54b0bd41
ertugdlk <ertgdlk@gmail.com>
2021-05-01 16:17:06 +0300

    pm2 settings

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 #give permission for everything in the up_service-app directory
-sudo chmod -R 777 /home/ec2-user/up_service
+sudo chmod -R 777 /home/ec2-user/
+
+#add npm and node to path
+export NVM_DIR="$HOME/.nvm"	
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
+
+export PATH=$PATH:/opt/node/lib/node_modules/pm2/bin
 
 #navigate into our working directory where we have all our bitbucket files
 cd /home/ec2-user/up_service
