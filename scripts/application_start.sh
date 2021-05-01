f1df470eac6c864b8991cb81f944f576ed3a2917
ertugdlk <ertgdlk@gmail.com>
2021-05-01 16:04:59 +0300

    last ci/cd settings

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -11,9 +11,9 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
-pm2 stop 0
 #install node modules
 npm install
+npm i up_core
 
 #start our node app in the background
 pm2 start 0
